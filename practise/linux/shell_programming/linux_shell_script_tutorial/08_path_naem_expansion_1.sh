d7b6da864282b91548766ab675f00ce67d5f7b25
satyanarayan <gullapallisatyanarayan777@gmail.com>
2017-01-09 20:13:58 +0530

    shell updated

---
+++ practise/linux/shell_programming/linux_shell_script_tutorial/08_path_naem_expansion_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo text1{ pattern1, pattern2, patternN }
+echo text2{pattern1, pattern2, patternN}
+echo text3{pattern1,pattern2,patternN}
+echo file1{1,2,3}.txt
+echo file2{1..5}.txt
---
+++ practise/linux/shell_programming/linux_shell_script_tutorial/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo text{ pattern1, pattern2, patternN }
+echo text{pattern1,pattern2,patternN}
+echo file{1,2,3}.txt
