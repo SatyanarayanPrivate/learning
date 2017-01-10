ca0274ad03f04b400a11693147ed6a7f3283e7c4
satyanarayan <gullapallisatyanarayan777@gmail.com>
2017-01-10 22:54:19 +0530

    shell program updated

---
+++ practise/linux/shell_programming/linux_shell_script_tutorial/10_exit_status_01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo ">>>>>>>>> exit status <<<<<<<<<<" 
+date1
+echo $?
+echo "----------------------------"
+ExitStatus=$?
+echo "command exit status $ExitStatus"
+echo "============================"
+echo ""
+
